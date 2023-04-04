0fd6fcde1feffce5b4d0c6fe7bc89961758604a5
stckwok <stckwok@hotmail.com>
2023-04-04 12:13:54 -0700

    compare dc power for 2 PV types)

--- sk_model/pv_modeling.py
+++ sk_model/pv_modeling.py
@@ -2,11 +2,12 @@
 
 import pvlib
 import pandas as pd
+import sys, getopt
 from matplotlib import pyplot as plt
 from pv_base import PVBase
 # from geopy.geocoders import Nominatim
 from tkinter import *
-import sys, getopt
+
 
 # a "typical meteorological year" (TMY) is composed of individual months
 # from several years, but to run a simulation we need
@@ -22,8 +23,10 @@ INVERTERS = pvlib.pvsystem.retrieve_sam('CECInverter')
 
 # It can be tricky to find the modules you want so you can visit their
 # GitHub page to search the CSV files manually, or use pvfree
-# https://pvfree.herokuapp.com/cec_modules/ and ditto for inverters
-# https://pvfree.herokuapp.com/pvinverters/
+# https://pvfree.azurewebsites.net/api/v1/cecmodule/
+# https://pvfree.azurewebsites.net/api/v1/pvinverters/
+# Filtering
+# https://pvfree.azurewebsites.net/api/v1/cecmodule/?{pv_params}&limit={limits}
 # NOTE: whitespace, hyphens, dashes, etc. are replaced by underscores
 # These are some basic 300-W Canadian Solar poly and mono Si modules
 
@@ -37,7 +40,7 @@ CECMOD_MONO_1 = CECMODS['Canadian_Solar_Inc__CS6X_300M']
 CECMOD_POLY_2 = CECMODS['Canadian_Solar_Inc__CS1H_325MS']
 CECMOD_MONO_2 = CECMODS['Canadian_Solar_Inc__CS1H_325MS']
 
-# here's a trick, transpose the database, and search the index using strings
+# Inverter - transpose the database, and search the index using strings
 INVERTERS.T[INVERTERS.T.index.str.startswith('SMA_America__STP')]
 #                                       Vac         Pso     Paco  ... Mppt_high    CEC_Date             CEC_Type
 # SMA_America__STP_60_US_10__480V_      480  116.969749  60000.0  ...     800.0         NaN  Utility Interactive
@@ -49,22 +52,23 @@ INVERTER_60K = INVERTERS['SMA_America__STP_60_US_10__480V_']
 # First, need to know where is the location (latitude, and longitude)
 # https://www.generateit.net/gps-coordinates/   <<< enter address to look up LAT, and LON
 # or
-# Use geopy APIs  (can not find it within IDE)
+# Use geopy APIs  (only work in commandline since it can not be installed to IDE)
 
 # LATITUDE, LONGITUDE = 40.5137, -108.5449
-#LATITUDE, LONGITUDE = 49.194793, -123.182710   # Vancouver International Airport
-#LATITUDE, LONGITUDE = 40.642422, -73.781749    # New York Airport
-#LATITUDE, LONGITUDE = 22.312130, 113.924857    # 1 Sky Plaza Rd, Chek Lap Kok, Hong Kong
+# LATITUDE, LONGITUDE = 49.194793, -123.182710   # Vancouver International Airport
+# LATITUDE, LONGITUDE = 40.642422, -73.781749    # New York Airport
+# LATITUDE, LONGITUDE = 22.312130, 113.924857    # 1 Sky Plaza Rd, Chek Lap Kok, Hong Kong
 # LATITUDE, LONGITUDE = 63.995339, -22.623854   # Iceland Airport
-# LATITUDE, LONGITUDE, LOCATION = 51.504473, 0.052271, "London Airport"       # London Airport
-# LATITUDE, LONGITUDE = 29.241639, 47.972874, "Newzealand Airport"
-# LATITUDE, LONGITUDE = 25.252747, 55.361275, "Dubai Airport"
+# LATITUDE, LONGITUDE = 51.504473, 0.052271,    # London Airport
+# LATITUDE, LONGITUDE = 29.241639, 47.972874,   # Newzealand Airport
+# LATITUDE, LONGITUDE = 25.252747, 55.361275,   # Dubai Airport
 
+# set address from user input
 address = ""
 
 
 def main(argv):
-    address = ''
+    # address = ''
     try:
         opts, args = getopt.getopt(argv, "ha:d:n:p:", ["ifile=", "ofile=", "lfile", "pfile"])
     except getopt.GetoptError:
@@ -83,17 +87,23 @@ def main(argv):
 
 
 class PVModeling(PVBase):
-        # Modelling steps are :
-        # * weather
-        # * solar position
-        # * PV surface orientation, aoi, etc.
-        # * plane of array irradiance
-        # * module temperatures
+        # Modelling steps are defined in https://pvpmc.sandia.gov/
+        # (1) weather
+        # (2) solar position
+        # (3) PV surface orientation, aoi, etc.
+        # (4) plane of array irradiance
+        # (5) module temperatures
+        # (6) DC output power
+        # (7) DC to AC conversion
 
         def set_test_params(self):
             # Override test parameters for your individual test.
             # This method must be overridden and num_nodes must be explicitly set.
             self.setup_clean_chain = True
+            self.mpp1 = ''
+            self.mpp2 = ''
+            self.tech1 = ''
+            self.tech2 = ''
 
         def get_solar_position(self, latitude, longitude):
             # get solar position
@@ -139,6 +149,15 @@ class PVModeling(PVBase):
             plt.ylabel('Energy [Wh/day]')
             plt.legend()
             plt.grid()
+            plt.show()
+
+        def plot_dc_compare(self, mpp1, mpp2, location, tech1="Mono-c-Si", tech2="Multi-c-Si"):
+            mpp1.p_mp.resample('D').sum().plot(figsize=(12, 8),
+                        label=tech1,
+                        title='Comparing Daily Energy ( {} )'.format(location))
+            mpp2.p_mp.resample('D').sum().plot(figsize=(12, 8), label=tech2)
+            plt.legend()
+            plt.ylabel('Production [Wh]')
             plt.show()
 
         def build_pv_array(self, cec_technology, temp_air, mpp):
@@ -277,6 +296,14 @@ class PVModeling(PVBase):
             # 7. calculate DC output
             mpp = self.get_mpp(cecparams)
             self.plot_dc_energy(cec_technology["Technology"], mpp, longitude, latitude, location)
+            if "Mono" in cec_technology["Technology"]:
+                print("Technology1 = Mono-c-Si")
+                self.mpp1 = mpp
+                self.tech1 = cec_technology["Technology"]
+            else:
+                print("Technology2 = Multi-c-Si")
+                self.mpp2 = mpp
+                self.tech2 = cec_technology["Technology"]
 
             # 8. calculate AC output
             edaily, ac_output = self.build_pv_array(cec_technology, temp_air, mpp)
@@ -292,6 +319,8 @@ class PVModeling(PVBase):
 
             cec_technology = CECMOD_POLY_1
             self.test_1(cec_technology, location=address, latitude=lat, longitude=long)
+
+            self.plot_dc_compare(self.mpp1, self.mpp2, address, self.tech1, self.tech2)
 
 
 if __name__ == '__main__':
